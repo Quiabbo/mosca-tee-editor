d62fc1a20fe233dd1a0db04b0d69f9cbbc4cc472
Filipi Hadji <filipi.hadji.dsg@gmail.com>
2026-05-12 13:09:08 -0300

    Fix: stable animejs v3 and total local cleanup v0.1.22

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import './index.css';
 import './i18n';
 import { HelmetProvider } from 'react-helmet-async';
 import * as agPsd from 'ag-psd';
-import { animate as anime } from 'animejs';
+import anime from 'animejs';
 
 // Attach to window for compatibility with code that expects global variables
 (window as any).agPsd = agPsd;
